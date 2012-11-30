ae0c3082030e807e616a94a28ec1380a7b6f159e
Tom <thejsjunky@gmail.com>
2012-11-30 02:39:29 -0500

    Added node code, reorganizing UI code

--- app/scripts/ui.js
+++ app/scripts/ui.js
@@ -1,154 +1,582 @@
-(function() {
-  
-  // ui namespace
-  window.ui = {};
-  
-  
-  // internal UI elements
-  var $window     = $( window );
-  var $dim        = null;
-  var $popup      = null;
-  var $popup_body = null;
-  
-  
-  /* Code Saving/Restoring */
-  
-  
-  ui.getSavedCode = function( name ) {
-    return localStorage.getItem( (name || "codebeats-last-code") );
-  }
-  
-  ui.saveCode = function( name ) {
-    localStorage.setItem( (name || "codebeats-last-code"), ui.cm.getValue() );
-  }
-  
-  
-  /* Resizing/Layout */
-  
-  
-  // *sizeCM*
-  // The CodeMirror editor should take up the available space and be at least 300px tall.
-  ui.sizeCM = function() {
-    var height = $window.height() - ui.$editor.position().top;
-    ui.cm.setSize( null, (height < 300  ? 300 : height) + "px" );
-  }
-  
-  // *centerPopup*
-  // Make sure the popup is centered on the page.
-  ui.centerPopup = function() {
-    var offset = {};
-    offset.left = ( $window.width() / 2 ) - ( $popup.width() / 2 );
-    offset.top  = ( $window.height() / 2 ) - ( $popup.height() / 2 );
-    $popup.offset( offset );
-  }
-  
-  
-  /* UI Widgets */
-  
-  ui.showPopup = function( $content_div ) {
-    $popup_body.empty();
-    $content_div = $content_div.clone();
-    $popup_body.append( $content_div );
-    $content_div.show();
-    $dim.show();
-    $popup.show();
-    ui.centerPopup();
-  }
-  
-  ui.hidePopup = function( ) {
-    $dim.hide();
-    $popup.hide();
-    $popup_body.empty();
-  }
-  
-  /* Utility Functions */
-  
-  ui.parseUrlArgs = function( args_str ) {
-    var args_ob  = {};
-    var params   = null;
-    if ( !args_str.length ) return false;
-    // eat leading # or ?
-    params = args_str.substr( 1 ).split( "&" );
-    params.forEach(function( val ) {
-      val = val.split( "=" );
-      if ( val.length === 2 ) args_ob[ val[0] ] = val[ 1 ];
-    });
-    return args_ob;
-  }
-  
-  
-  /* GitHub Gist Integration */
+/*****************************************************************************
+*
+*                  CodeBeats : Online Music Coding Platform
+*
+*  This file is part of the CodeBeats project. The project is distributed at:
+*  https://github.com/maximecb/CodeBeats
+*
+*  Copyright (c) 2012, Maxime Chevalier-Boisvert
+*  All rights reserved.
+*
+*  This software is licensed under the following license (Modified BSD
+*  License):
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions are
+*  met:
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*    * Redistributions in binary form must reproduce the above copyright
+*      notice, this list of conditions and the following disclaimer in the
+*      documentation and/or other materials provided with the distribution.
+*    * Neither the name of the Universite de Montreal nor the names of its
+*      contributors may be used to endorse or promote products derived
+*      from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+*  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+*  TO, THE IMPLIED WARRANTIES OF MERCHApNTABILITY AND FITNESS FOR A
+*  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UNIVERSITE DE
+*  MONTREAL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+*  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+*  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+*  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+*  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+*  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+*  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*****************************************************************************/
 
-  
-  
-  /* Event Handling, Init, Etc */
-  
-  // init
-  // Initialize the UI
-  function init() {
+//============================================================================
+// UI Liv Code
+//============================================================================
+//(function()
+//{
 
-    console.log('ui init function');
+    /*      DOM HELPERS     */
 
-    // initialize locals
-    $dim        = $( "#dim" );
-    $popup      = $( "#popup" );
-    $popup_body = $( "#popup-content" );
-    
-    // initialize ui properties
-    ui.$editor = $( "#editor" );
-    ui.editor  = ui.$editor.get( 0 );
-    
-    // create code mirror editor
-    ui.cm = CodeMirror( ui.editor, {
-       value: ui.getSavedCode()
-      ,mode:  "javascript"
-      ,theme: "monokai"
-      ,lineNumbers: true
-    });
-    
-    // Handle GH auth
-    
-    
-    // Update UI
-    ui.sizeCM();
 
-    // ui event listeners
-    $( "#popup-close" ).click( function(){
-      ui.hidePopup();
-      return false;
-    });
-    
-    // test
-    // TODO: remove
-    $( "#load-btn" ).click( function() {
-      ui.showPopup( $("#open-form") );
-    });
 
-    // play/pause/stop buttons
-    $('#play-btn').click(function () {    
-        $('#play-btn').attr('disabled', 'disabled');
-        $('#pause-btn').removeAttr('disabled');
-        $('#stop-btn').removeAttr('disabled');
-        playAudio(ui.cm.getValue());
-    });
-    $('#pause-btn').click(function () {
-        $('#play-btn').removeAttr('disabled');
-        $('#pause-btn').attr('disabled', 'disabled');
-        $('#stop-btn').removeAttr('disabled');
-        pauseAudio(); 
-    });
-    $('#stop-btn').click(function () { 
-        $('#play-btn').removeAttr('disabled');
-        $('#pause-btn').attr('disabled', 'disabled');
-        $('#stop-btn').attr('disabled', 'disabled');
-        stopAudio(); 
-    });
-  }
-  
-  // Window event listeners
-  $( init );
-  $window.unload( ui.saveCode );
-  $window.resize( ui.sizeCM );
-  $window.resize( ui.centerPopup );
-  
-})();
+    /*
+    Take an object of name/selector pairs and
+    create jQuery objects from them
+    */
+    function makeJQObjects()
+    {
+        for (var id in $el)
+        {
+            if ($el.hasOwnProperty(id)) $el[id] = $($el[id]);
+        }
+    }
+    
+    /*
+    Make a function that toggles the buttons
+    */
+    function makeToggler(play, pause, stop)
+    {
+        return function ()
+        {
+            $el.toolbar_btns.attr('disabled', 'disabled');
+            if (play) $el.play_btn.removeAttr('disabled');
+            if (pause) $el.pause_btn.removeAttr('disabled');
+            if (stop) $el.stop_btn.removeAttr('disabled');
+        }
+    }
+    
+    /**
+    Make a function that will display the given error message.
+    Useful in callbacks, etc. Handler is a callback that will be called.
+    */
+    function makeErrorMsg(msg, handler)
+    {
+        return function ()
+        {
+            $el.dim.show();
+            $el.error_msg.show();
+            $el.error_msg_text.text(msg);
+            centerPopup();
+            if (handler) handler();
+        }
+    }
+
+
+
+    /*      Resizing/Layout     */
+
+
+
+    /**
+    The CodeMirror editor should take up the available space
+    and be at least 400px tall.
+    */
+    function sizeCM()
+    {
+        var height = $window.height() - $el.editor.position().top;
+        ui.cm.setSize(null, (height < 400  ? 400 : height) + "px");
+    }
+
+    /**
+    Make sure the popup is centered on the page.
+    */
+    function centerPopup()
+    {
+        var $popup = $el.popups.filter(":visible").first();
+        var offset = {};
+        offset.left = ($window.width() / 2) - ($popup.width() / 2);
+        offset.top  = ($window.height() / 2) - ($popup.height() / 2);
+        $popup.offset(offset);
+    }
+
+
+
+    /*      UI Widgets      */
+
+
+
+    /**
+    Display a popover populated by $content_div
+    */
+    function showPopup($popup) {
+        $el.dim.show();
+        $popup.show();
+        centerPopup();
+    }
+
+    /**
+    Hide the popup displayed with showPopup()
+    */
+    function hidePopup()
+    {
+        $el.dim.hide();
+        $el.popups.hide();
+        return false;
+    }
+
+
+
+    /*      Code Saving/Restoring       */
+
+
+    /**
+    Get the code in localStorage
+    */
+    function getSavedCode(name)
+    {
+        return localStorage.getItem(name || "codebeats-last-code");
+    }
+
+    /**
+    Save the code in localStorage
+    */
+    function saveCode(name)
+    {
+        localStorage.setItem((name || "codebeats-last-code"), ui.cm.getValue());
+    }
+
+
+
+    /*      Utility Functions       */
+
+
+
+    /**
+    Parse a string like location.hash or location.query into an object
+    */
+    function parseUrlArgs(args_str)
+    {
+        var args_ob = {};
+        var params = null;
+        if ( !args_str.length ) return false;
+
+        // eat leading # or ?
+        params = args_str.substr(1).split("&");
+        var val = null;
+        var i = params.length;
+        while (i--)
+        {
+            val = params[i].split("=");
+            if (val.length === 2) args_ob[val[0]] = val[1];
+        }
+
+        return args_ob;
+    }
+
+    /**
+    Parse the document.cookie string into an object
+    */
+    function parseCookies()
+    {
+        var args_ob = {};
+        var cookies = document.cookie;
+        if (!cookies.length) return false;
+        
+        cookies = cookies.split(";");
+        cookies.forEach(function(val)
+        {
+            val = val.split("=");
+            if (val.length === 2) args_ob[val[0]] = val[1];
+        });
+        return args_ob;
+    }
+
+    /**
+    Clear out any hash args and cookies we don't want
+     */
+    function clearAuthArgs()
+    {
+        // TODO: have this replace any non-auth related args
+        window.location.hash = "";
+        document.cookie = "gha=" + no_cookie;
+    }
+
+    /**
+    Log the user out, destroying any copies of the auth token
+    and reloading the page
+     */
+    function logOut()
+    {
+        atoken = null;
+        logged_in = false;
+        clearAuthArgs()
+        window.location.reload();
+        return false;
+    }
+
+    /* GitHub Gist Integration */
+
+
+    /**
+    Get the gists that have been saved to recently.
+    */
+    function getRecentGists()
+    {
+        var recent = localStorage.getItem("codebeats-recent-gists");
+        return (recent) ? recent.split(",") : [];
+    }
+
+    /**
+    Add a gist to the recently-saved-to list.
+    */
+    function saveRecentGist(gist)
+    {
+        var recent = getRecentGists();
+        recent.push( gist );
+        recent = recent.join(",");
+        return localStorage.setItem("codebeats-recent-gists", recent);
+    }
+    
+    /**
+    Display recently saved gist in a select (used by multiple forms)
+    */
+    function displayRecentGists($select, $nag)
+    {
+        var recent = getRecentGists();
+        var length = recent.length;
+        if (length)
+        {
+            $select.empty();
+            while (length--)
+            {
+                $select.prepend("<option>" + recent[length] + "</option>")
+            }
+            $nag.hide();
+            $select.show();
+        }
+        else
+        {
+            $nag.show();
+            $select.hide();
+        }
+    }
+
+
+    /**
+    Make a request for the info for a specific gist
+    */
+    function getGistOb(gist_id)
+    {
+        var req = $.get(
+            "https://api.github.com/gists/" + gist_id + "?" +
+            "access_token=" + atoken + "&" +
+            "client_id=" + "<% CLIENT_ID %>"
+            , post_data);
+        return req;
+    }
+    
+    function getGistFiles(id)
+    {
+        var req = new $.Deferred();
+        
+        getGistOb()
+            .success(function(data){
+                req.resolve(data.files);
+            })
+            .fail(function(){
+                alert("ERROR!");
+            })
+    }
+
+    
+    function saveSnippet( gist_id, file_name, content )
+    {
+        var files = {}
+        files[ file_name ] = { content: content };
+
+        var post_data = {
+            "public": true,
+            description : "Created by CodeBeats.",
+            files: files,
+        }
+        post_data = JSON.stringify(post_data);
+
+        // make the GET request
+        var req = $.post(
+        "https://api.github.com/gists?" +
+        "access_token=" + atoken + "&" +
+        "client_id=" + "<% CLIENT_ID %>"
+        , post_data);
+
+        // success
+        req.success(function(data)
+        {
+            console.log(data);
+        });
+
+        // failure
+        req.fail(function ()
+        {
+            // TODO: better errors
+            alert("ERROR!");
+        });
+        return false;
+    }
+
+    function handleSave() {
+        saveSnippet( null, "cb.js", ui.cm.getValue() );
+        hidePopup();
+        return false;
+    }
+    
+    
+    /**
+    Retrieve info about the user; name, gravatar, etc
+    */
+    function getUserInfo()
+    {
+        // the data for the login GET request
+        // NOTE: <% CLIENT_ID %> must be replaced by a real
+        // one before deployment
+        var data = {
+            access_token: atoken,
+            client_id: "<% CLIENT_ID %>"
+        }
+
+        // make the GET request
+        var req = $.get("https://api.github.com/user", data);
+
+        // success
+        req.success(function(data)
+        {
+            user_info = data;
+            showUserInfo();
+        });
+
+        // failure
+        req.fail(function ()
+        {
+            // TODO: better errors
+            alert("ERROR!");
+        });
+    }
+    
+    /**
+    Display the user info in a little badge in the toolbar
+    */
+    function showUserInfo()
+    {
+        if (!user_info) return false;
+        $("#github-login").hide();
+        $("#user-info").html("" +
+            "<a href='#' id='log-out'>Log Out</a>" + 
+            "<small>" + user_info.login + "</small>" +
+            "<img src='" + user_info.avatar_url +
+            "'>" + 
+            "").show();
+        $('#log-out').click(logOut);
+        return true;
+    }
+
+//============================================================================
+// UI Main
+//============================================================================
+
+
+    /**
+    ui namespace
+    */
+    window.ui = {};
+    
+    /**
+    Internal UI state/vars
+    */
+    
+    // codemirror instance
+    var cm = null;
+    // github auth token
+    var atoken  = null;
+    // whether the user is logged into GitHub
+    var logged_in = false;
+    // gihub user info
+    var user_info = null;
+    // bad hacker, no cookie
+    var no_cookie  = "(ಠ_ಠ)"
+
+    // ui elements
+    var $el = {
+        // dims the background for popups
+        dim : "#dim",
+        // all the popup containers
+        popups : ".popup",
+        // popup for error messages
+        error_msg : "#error-msg",
+        // container for error text
+        error_msg_text :"#error-msg-text",
+        // the container for the CM editor
+        editor : "#editor",
+        // play button in toolbar
+        play_btn : "#play-btn",
+        // pause button in toolbar
+        pause_btn : "#pause-btn",
+        // stop button in toolbar
+        stop_btn : "#stop-btn",
+        // all the toolbar buttons
+        toolbar_btns : ".toolbar-btn",
+        // link on Load Gist popup
+        load_local_link : "#load-local",
+        // link on Save Gist popup
+        save_local_link : "#save-local"
+    }
+
+    /**
+    Initialize all ui functions, elements, etc
+    */
+    function init() {
+        // make jQuery objects for all the elements we need upfront
+        makeJQObjects();
+
+        // create code mirror editor
+        ui.cm = CodeMirror($el.editor.get(0), {
+            value: getSavedCode(),
+            mode: "javascript",
+            theme: "monokai",
+            lineNumbers: true
+        });
+
+        /**
+        Update UI
+        */
+        sizeCM();
+        
+        /**
+        User/Github initializations
+        */
+        if (logged_in) getUserInfo();
+
+        /**
+        ui event listeners
+        */
+
+        // close buttons shared by all popup
+        $(".popup-close").click(hidePopup);
+        $(".cancel-btn").click(hidePopup);
+
+        // save Gist button
+        $("#save-btn").click( function() {
+            showPopup( $("#save-form") );
+            return false;
+        });
+        
+        // open Gist button
+        $("#load-btn").click( function() {
+            showPopup( $("#open-form") );
+            // TODO: cache jQuery objects
+            displayRecentGists($("#open-recent"), $("#open-no-recent"));
+            return false;
+        });
+        
+
+        // the "just save what I have locally" link
+        $el.save_local_link.click(function ()
+        {
+            saveCode();
+            return hidePopup();
+        });
+        
+        // the "just load what I have saved locally" link
+        $el.load_local_link.click(function ()
+        {
+            ui.cm.setValue(getSavedCode());
+            return hidePopup();
+        });
+
+        // play
+        $el.play_btn.click(makeToggler(false,true,true));
+        $el.play_btn.click(function ()
+        {
+            playAudio(ui.cm.getValue());
+        });
+
+        // pause
+        $el.pause_btn.click(makeToggler(true,false,true));
+        $el.pause_btn.click(function ()
+        {
+            pauseAudio();
+        });
+
+        // stop
+        $el.stop_btn.click(makeToggler(true,false,false));
+        $el.stop_btn.click(function ()
+        {
+            stopAudio();
+        });
+    }
+
+
+    /**
+    Teardown the UI; save any code and if logged in save the auth token to
+    a cookie.
+    */
+    function unload()
+    {
+        saveCode();
+        if (atoken && logged_in)
+            document.cookie = "gha=" + atoken;
+    }
+
+
+    /*      Handle Auth and UI pre-initialization       */
+
+    // first check if we already have an auth cookie
+    var cookies = parseCookies();
+    // if there is an auth cookie, it needs to be overwritten for the life
+    // of the page so it can't be accessed from eval()'d code
+    if (cookies && cookies.gha && cookies.gha !== no_cookie) {
+        atoken = cookies.gha;
+        logged_in = true;
+        clearAuthArgs()
+    } else {
+        // parse auth token if any
+        // TODO: this needs to be done earlier to grab any
+        // gist=&file= args
+        var args = parseUrlArgs(window.location.hash);
+        if (args.access_token)
+        {
+            atoken = args.access_token;
+            logged_in = true;
+            clearAuthArgs();
+        }
+    }
+
+
+
+    /**
+    Window event listeners
+    */
+    var $window = $(window);
+
+    $(init);
+    $window.unload(unload);
+    $window.resize(sizeCM);
+    $window.resize(centerPopup);
+
+//})();
--- web.js
+++ web.js
@@ -1,1 +1,172 @@
-// node.js code here
+/*****************************************************************************
+*
+*                  CodeBeats : Online Music Coding Platform
+*
+*  This file is part of the CodeBeats project. The project is distributed at:
+*  https://github.com/maximecb/CodeBeats
+*
+*  Copyright (c) 2012, Maxime Chevalier-Boisvert
+*  All rights reserved.
+*
+*  This software is licensed under the following license (Modified BSD
+*  License):
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions are
+*  met:
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*    * Redistributions in binary form must reproduce the above copyright
+*      notice, this list of conditions and the following disclaimer in the
+*      documentation and/or other materials provided with the distribution.
+*    * Neither the name of the Universite de Montreal nor the names of its
+*      contributors may be used to endorse or promote products derived
+*      from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+*  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+*  TO, THE IMPLIED WARRANTIES OF MERCHApNTABILITY AND FITNESS FOR A
+*  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UNIVERSITE DE
+*  MONTREAL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+*  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+*  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+*  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+*  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+*  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+*  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*****************************************************************************/
+
+//============================================================================
+// Server-Side Code
+//============================================================================
+
+
+/**
+Module Imports
+*/
+var express = require('express');
+// TODO: github docs say https is needed, test this
+var http    = require('https');
+
+/**
+The options for the POST to get an auth token
+*/
+var auth_req_ops = {
+    host: 'github.com',
+    path: '/login/oauth/access_token',
+    method: 'POST'
+};
+
+
+/**
+Application Server (handles all http requests)
+*/
+var app = express.createServer(express.logger());
+
+/**
+Configure app and middleware
+*/
+app.configure(function()
+{
+
+    // middleware to exchange code for token
+    app.use( '/auth/', function(req, res, next)
+    {
+
+        // TODO: error handling; no "code" parameter etc
+
+        var code = req.query["code"];
+
+        // note: <% CLIENT_ID %> & <% SECRET_ID %> must be replaced
+        // with real ones before deployment
+        var post_data =
+            "client_id=<% CLIENT_ID %>&client_secret=<% SECRET_ID %>&state=xviv&code="
+            + code;
+
+        // need to set Content-Length
+        auth_req_ops.headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': post_data.length
+        }
+        
+        // the request object for the POST
+        var auth_req = http.request( auth_req_ops, function ( auth_response )
+        {
+            // token_str will hold the auth token
+            var token_str = '';
+
+            auth_response.setEncoding('utf8');
+
+            // getting data from GitHub
+            auth_response.on('data', function (chunk)
+            {
+                token_str += chunk;
+            });
+
+            // TODO: need on.close() handler?
+
+            // request done, add token to request object and call
+            // next middleware
+            auth_response.on('end', function ()
+            {
+                req.token = token_str;
+                next();
+            });
+        });
+
+        // request for token errored
+        auth_req.on('error', function (e)
+        {
+            // TODO: error handling?
+            next();
+        });
+
+        // send POST
+        auth_req.write(post_data);
+        auth_req.end();
+    });
+
+
+    // if not /auth/ fallback on the static server middleware
+    // which will just serve static files from /app
+    // TODO: app manifest needs to be sent with special headers
+    //       implement in another middleware?
+    app.use(express.static(__dirname + '/app'));
+});
+
+
+/**
+Handlers for app
+*/
+
+// The GitHub API will redirect back to /auth/ by the time the request
+// hits this handler the middleware has already retrieved an auth token
+// and added it to the reguest
+app.get('/auth/', function (req, res)
+{
+    if (req.token)
+    {
+        // append token to hash and redirect
+        res.redirect("/#" + req.token);
+    }
+    else
+    {
+        // TODO: redirect with error message
+        res.send("doh!");
+    }
+});
+
+
+/**
+Initialize Server
+*/
+
+// heroku puts the appropriate port in env.PORT
+var port = process.env.PORT || 5000;
+
+// start server
+app.listen(port, function ()
+{
+    console.log("Listening on:\t" + port);
+});
